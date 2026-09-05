215e638b387dbb166d2820ad745bb2ff4e3c9eef
Bence Dániel Erős <61280717+erosbence@users.noreply.github.com>
2026-09-06 01:14:10 +0200

    Add firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// A Firebase Console > Project settings > Your apps > Web app alatt kapott értékek.
+export const firebaseConfig = {
+  apiKey: "PASTE_FIREBASE_API_KEY_HERE",
+  authDomain: "PASTE_PROJECT_ID_HERE.firebaseapp.com",
+  projectId: "PASTE_PROJECT_ID_HERE",
+  storageBucket: "PASTE_PROJECT_ID_HERE.firebasestorage.app",
+  messagingSenderId: "PASTE_MESSAGING_SENDER_ID_HERE",
+  appId: "PASTE_APP_ID_HERE",
+};
